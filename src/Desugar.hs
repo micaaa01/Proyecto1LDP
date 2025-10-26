99ce63d49becb7a2b98e2dadfdd0c42e0dc55105
micaaa01 <159498443+micaaa01@users.noreply.github.com>
2025-10-25 22:27:38 -0600

    Refactor desugar function and ASA data constructors

--- src/Desugar.hs
+++ src/Desugar.hs
@@ -4,40 +4,40 @@ import Grammar (SASA(..))
 data ASA = NumS Int
                 | BooleanS Bool
                 | IdS String
-                | AddS ASA ASA
-                | SubS ASA ASA
-                | MulS ASA ASA
-                | DivS ASA ASA
-                | SqrtS ASA
-                | ExptS ASA ASA
-                | NotS ASA
-                | FstS ASA
-                | SndS ASA
-                | LetRecS String ASA ASA   --No se que hace este
-                | LetCCS String ASA   --No se que hace este 
-                | IfS ASA ASA ASA
-                | FunS String ASA
-                | AppS ASA ASA
+                | Add ASA ASA
+                | Sub ASA ASA
+                | Mul ASA ASA
+                | Div ASA ASA
+                | Sqrt ASA
+                | Expt ASA ASA
+                | Not ASA
+                | Fst ASA
+                | Snd ASA
+                | LetRec String ASA ASA   --No se que hace este
+                | LetCC String ASA   --No se que hace este 
+                | If ASA ASA ASA
+                | Fun String ASA
+                | App ASA ASA
                 deriving (Show, Eq)
 
 desugar :: SASA -> ASA
-desugar (Num n)             = NumS n
-desugar (Boolean b)         = BooleanS b
-desugar (Id x)              = IdS x
-desugar (Add e1 e2)         = AddS (desugar e1) (desugar e2)
-desugar (Sub e1 e2)         = SubS (desugar e1) (desugar e2)
-desugar (Not e)             = NotS (desugar e)
-desugar (Mul e1 e2)         = MulS (desugar e1) (desugar e2)
-desugar (Div e1 e2)         = DivS (desugar e1) (desugar e2)
-desugar (Sqrt e)            = SqrtS (desugar e)
-desugar (Expt e1 e2)        = ExptS (desugar e1) (desugar e2)
-desugar (Fst e)             = FstS (desugar e)
-desugar (Snd e)             = SndS (desugar e)
-desugar (If c t f)          = IfS (desugar c) (desugar t) (desugar f)
-desugar (Fun x body)        = FunS x (desugar body)
-desugar (App e1 e2)         = AppS (desugar e1) (desugar e2)
+desugar (Num n)             = Num n
+desugar (Boolean b)         = Boolean b
+desugar (Id x)              = Id x
+desugar (Add e1 e2)         = Add (desugar e1) (desugar e2)
+desugar (Sub e1 e2)         = Sub (desugar e1) (desugar e2)
+desugar (Not e)             = Not (desugar e)
+desugar (Mul e1 e2)         = Mul (desugar e1) (desugar e2)
+desugar (Div e1 e2)         = Div (desugar e1) (desugar e2)
+desugar (Sqrt e)            = Sqrt (desugar e)
+desugar (Expt e1 e2)        = Expt (desugar e1) (desugar e2)
+desugar (Fst e)             = Fst (desugar e)
+desugar (Snd e)             = Snd (desugar e)
+desugar (If c t f)          = If (desugar c) (desugar t) (desugar f)
+desugar (Fun x body)        = Fun x (desugar body)
+desugar (App e1 e2)         = App (desugar e1) (desugar e2)
 desugar (Let [] body) = desugar body
-desugar (Let [(x, e)] body) = AppS (FunS x (desugar body)) (desugar e)
+desugar (Let [(x, e)] body) = App (Fun x (desugar body)) (desugar e)
 desugar (Let ((x,e):rest) body) = desugar (Let [(x,e)] (Let rest body))
 desugar (LetStar [] body) = desugar body
 desugar (LetStar ((x,e):rest) body) = desugar (Let [(x,e)] (LetStar rest body))
