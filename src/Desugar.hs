ad9f54e6a66e6bf9dcd4983ad0221dc41744af69
micaaa01 <159498443+micaaa01@users.noreply.github.com>
2025-10-25 22:36:08 -0600

    Update Desugar.hs

--- src/Desugar.hs
+++ src/Desugar.hs
@@ -2,8 +2,8 @@ module Desugar where
 import Grammar (SASA(..))
 
 data ASA = Num Int
-                | BooleanS Bool
-                | IdS String
+                | Boolean Bool
+                | Id String
                 | Add ASA ASA
                 | Sub ASA ASA
                 | Mul ASA ASA
@@ -21,23 +21,23 @@ data ASA = Num Int
                 deriving (Show, Eq)
 
 desugar :: SASA -> ASA
-desugar (Num n)             = Num n
-desugar (Boolean b)         = Boolean b
-desugar (Id x)              = Id x
-desugar (Add e1 e2)         = Add (desugar e1) (desugar e2)
-desugar (Sub e1 e2)         = Sub (desugar e1) (desugar e2)
-desugar (Not e)             = Not (desugar e)
-desugar (Mul e1 e2)         = Mul (desugar e1) (desugar e2)
-desugar (Div e1 e2)         = Div (desugar e1) (desugar e2)
-desugar (Sqrt e)            = Sqrt (desugar e)
-desugar (Expt e1 e2)        = Expt (desugar e1) (desugar e2)
-desugar (Fst e)             = Fst (desugar e)
-desugar (Snd e)             = Snd (desugar e)
-desugar (If c t f)          = If (desugar c) (desugar t) (desugar f)
-desugar (Fun x body)        = Fun x (desugar body)
-desugar (App e1 e2)         = App (desugar e1) (desugar e2)
-desugar (Let [] body) = desugar body
-desugar (Let [(x, e)] body) = App (Fun x (desugar body)) (desugar e)
-desugar (Let ((x,e):rest) body) = desugar (Let [(x,e)] (Let rest body))
-desugar (LetStar [] body) = desugar body
-desugar (LetStar ((x,e):rest) body) = desugar (Let [(x,e)] (LetStar rest body))
+desugar (NumS n)             = Num n
+desugar (BooleanS b)         = Boolean b
+desugar (IdS x)              = Id x
+desugar (AddS e1 e2)         = Add (desugar e1) (desugar e2)
+desugar (SubS e1 e2)         = Sub (desugar e1) (desugar e2)
+desugar (NotS e)             = Not (desugar e)
+desugar (MulS e1 e2)         = Mul (desugar e1) (desugar e2)
+desugar (DivS e1 e2)         = Div (desugar e1) (desugar e2)
+desugar (SqrtS e)            = Sqrt (desugar e)
+desugar (ExptS e1 e2)        = Expt (desugar e1) (desugar e2)
+desugar (FstS e)             = Fst (desugar e)
+desugar (SndS e)             = Snd (desugar e)
+desugar (IfS c t f)          = If (desugar c) (desugar t) (desugar f)
+desugar (FunS x body)        = Fun x (desugar body)
+desugar (AppS e1 e2)         = App (desugar e1) (desugar e2)
+desugar (LetS [] body) = desugar body
+desugar (LetS [(x, e)] body) = App (Fun x (desugar body)) (desugar e)
+desugar (LetS ((x,e):rest) body) = desugar (Let [(x,e)] (Let rest body))
+desugar (LetStarS [] body) = desugar body
+desugar (LetStarS ((x,e):rest) body) = desugar (Let [(x,e)] (LetStar rest body))
