42576323b209434c9628724afcec4e8c5fc74cfe
unknown <pollispaps49@gmail.com>
2025-10-27 13:32:35 -0600

    Main corregido

--- src/Main.hs
+++ src/Main.hs
@@ -2,29 +2,37 @@
 module Main where
 
 import Lexer (lexer, Token(..))
-import Grammar (parser, ASA(..))
-import Desugar (desugar, SASA(..))
+import Grammar (parser, SASA(..))
+import Desugar (desugar, ASA(..))
+import Control.Exception (catch, SomeException)
 
 -- Ejemplo de expresión en tu lenguaje
-input :: String
-input = "(let ((x 5) (y 10)) (+ x y))"
+-- input :: String
+-- input = "(let ((x 5) (y 10)) (+ x y))"
 
 main :: IO ()
 main = do
-    putStrLn "Entrada original:"
-    putStrLn input
+    putStrLn "Ingresa una expresión MINILISP:"
+    input <- getLine
+    putStrLn ("\nEntrada original:\n" ++ input)
 
-    -- Paso 1: Lexer
-    let tokens = lexer input
-    putStrLn "\nTokens generados por el lexer:"
-    print tokens
+    catch (do
+        -- Paso 1: Lexer
+        let tokens = lexer input
+        putStrLn "\nTokens generados por el lexer:"
+        print tokens
 
-    -- Paso 2: Parser
-    let ast = parser tokens
-    putStrLn "\nAST generado por el parser (ASA):"
-    print ast
+        -- Paso 2: Parser
+        let ast = parser tokens
+        putStrLn "\nAST generado por el parser (SASA):"
+        print ast
 
-    -- Paso 3: Desugar
-    let sasaAst = desugar ast
-    putStrLn "\nAST desazucarizado (SASA):"
-    print sasaAst
+        -- Paso 3: Desugar
+        let sasaAst = desugar ast
+        putStrLn "\nAST desazucarizado (ASA):"
+        print sasaAst
+      )
+      handler
+
+handler :: SomeException -> IO ()
+handler e = putStrLn $ "\n Error durante la ejecución: " ++ show e
