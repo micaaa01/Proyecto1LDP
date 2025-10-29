0e2f28b8b0984d834003beb3ac5bd83a37d8221b
unknown <pollispaps49@gmail.com>
2025-10-29 15:33:00 -0600

    Exponencial Variadico

--- src/Grammar.y
+++ src/Grammar.y
@@ -45,65 +45,55 @@ import Lexer (Token(..))
 %%
 
 SASA 
-      : int                          { NumS $1 }
-      | bool                         { BooleanS $1 }
-      | var                          { IdS $1 }
-      | '(' "not" SASA ')'            { NotS $3 }
-      | '(' '+' ExpList ')'   { AddListS $3 }
-      | '(' '-' ExpList ')'   { SubListS $3 }
-      | '(' '*' ExpList ')'   { MulListS $3 }
-      | '(' '/' ExpList ')'   { DivListS $3 }
-      | '(' '=' ExpList ')'   { EqListS $3 }
-      | '(' '<' ExpList ')'   { LtListS $3 }
-      | '(' '>' ExpList ')'   { GtListS $3 }
-      | '(' '<=' ExpList ')'       { LeListS $3 }
-      | '(' '>=' ExpList ')'       { GeListS $3 }
-      | '(' '!=' ExpList ')'       { NeListS $3 }
-      | '(' "lambda" '(' VarList ')' SASA ')' { FunListS $4 $6 }
-      | '(' "sqrt" SASA ')'           { SqrtS $3 }
-      | '(' "expt" SASA SASA ')'       { ExptS $3 $4 }
-      | '(' "fst" SASA ')'            { FstS $3 } 
-      | '(' "snd" SASA ')'            { SndS $3 }
-      | '(' SASA ',' SASA ')' { PairS $2 $4 }
-      | '(' "if" SASA SASA SASA ')'     { IfS $3 $4 $5 }
-      | '(' "let" '(' Bindings ')' SASA ')'    { LetS $4 $6 }
-      | '(' "let*" '(' Bindings ')' SASA ')'   { LetStarS $4 $6 }
-      | '[' ListItems ']'            { ListS $2 }
-      | '[' ']'                           { ListS [] }
-      | '(' "head" SASA ')'          { HeadS $3 }
-      | '(' "tail" SASA ')'          { TailS $3 }
---      | '(' var CondClauses ')' { parseCondOrApp $2 $3 } 
-      | '(' "cond" CondClauses ')' { CondS $3 }
---      | '(' SASA SASA ')'              { AppS $2 $3 }
-      | '(' SASA SASAList ')' { foldl AppS $2 $3 }
-      | '(' "add1" SASA ')'          { AddListS [$3, NumS 1] }
-      | '(' "sub1" SASA ')'          { SubListS [$3, NumS 1] }
+      : int                                                 { NumS $1 }
+      | bool                                                { BooleanS $1 }
+      | var                                                 { IdS $1 }
+      | '(' "not" SASA ')'                                  { NotS $3 }
+      | '(' '+' ExpList ')'                                 { AddListS $3 }
+      | '(' '-' ExpList ')'                                 { SubListS $3 }
+      | '(' '*' ExpList ')'                                 { MulListS $3 }
+      | '(' '/' ExpList ')'                                 { DivListS $3 }
+      | '(' '=' ExpList ')'                                 { EqListS $3 }
+      | '(' '<' ExpList ')'                                 { LtListS $3 }
+      | '(' '>' ExpList ')'                                 { GtListS $3 }
+      | '(' '<=' ExpList ')'                                { LeListS $3 }
+      | '(' '>=' ExpList ')'                                { GeListS $3 }
+      | '(' '!=' ExpList ')'                                { NeListS $3 }
+      | '(' "lambda" '(' VarList ')' SASA ')'               { FunListS $4 $6 }
+      | '(' "sqrt" SASA ')'                                 { SqrtS $3 }
+      | '(' "expt" ExpList ')' { ExptListS $3 }
+      | '(' "fst" SASA ')'                                  { FstS $3 } 
+      | '(' "snd" SASA ')'                                  { SndS $3 }
+      | '(' SASA ',' SASA ')'                               { PairS $2 $4 }
+      | '(' "if" SASA SASA SASA ')'                         { IfS $3 $4 $5 }
+      | '(' "let" '(' Bindings ')' SASA ')'                 { LetS $4 $6 }
+      | '(' "let*" '(' Bindings ')' SASA ')'                { LetStarS $4 $6 }
+      | '[' ListItems ']'                                   { ListS $2 }
+      | '[' ']'                                             { ListS [] }
+      | '(' "head" SASA ')'                                 { HeadS $3 }
+      | '(' "tail" SASA ')'                                 { TailS $3 }
+      | '(' "cond" CondClauses ')'                          { CondS $3 }
+      | '(' SASA SASAList ')'                               { foldl AppS $2 $3 }
+      | '(' "add1" SASA ')'                                 { AddListS [$3, NumS 1] }
+      | '(' "sub1" SASA ')'                                 { SubListS [$3, NumS 1] }
 
 
 --Esta es un auxiliar      
 ExpList
-      : SASA                  { [$1] }
-      | SASA ExpList          { $1 : $2 }
+      : SASA                        { [$1] }
+      | SASA ExpList                { $1 : $2 }
 
 VarList
-      : var                { [$1] }
-      | var VarList        { $1 : $2 }
+      : var                         { [$1] }
+      | var VarList                 { $1 : $2 }
 
 ListItems
       : SASA                        { [$1] }
-      | SASA ',' ListItems         { $1 : $3 }
+      | SASA ',' ListItems          { $1 : $3 }
 
--- CondClauses
---    : '[' SASA SASA ']'                     { [($2, $3)] }
---    | '[' SASA SASA ']' CondClauses         { ($2, $3) : $5 }
---    | '[' var SASA ']' 
---        { case $2 of
---            "else" -> [(BooleanS True, $3)]
---            _      -> error "Cláusula cond mal formada: se esperaba 'else'"
---        }
 
 CondClause
-    : '[' SASA SASA ']'         { ($2, $3) }
+    : '[' SASA SASA ']'             { ($2, $3) }
     | '[' var SASA ']' 
         { case $2 of
             "else" -> (BooleanS True, $3)
@@ -111,22 +101,22 @@ CondClause
         }
 
 CondClauses
-    : CondClause                { [$1] }
-    | CondClause CondClauses    { $1 : $2 }
+    : CondClause                    { [$1] }
+    | CondClause CondClauses        { $1 : $2 }
 
       
 SASAList
-  : SASA              { [$1] }
-  | SASA SASAList     { $1 : $2 }
+  : SASA                            { [$1] }
+  | SASA SASAList                   { $1 : $2 }
 
 
 
 Bindings
-      : Binding                    { [$1] }
+      : Binding                     { [$1] }
       | Binding Bindings            { $1 : $2 }
 
 Binding
-      : '(' var SASA ')'             { ($2, $3) }
+      : '(' var SASA ')'            { ($2, $3) }
 
 
 
@@ -150,6 +140,7 @@ data SASA
       | FunListS [String] SASA
       | SqrtS SASA
       | ExptS SASA SASA
+      | ExptListS [SASA]
       | FstS SASA
       | SndS SASA
       | PairS SASA SASA
@@ -168,8 +159,5 @@ data SASA
 parseError :: [Token] -> a
 parseError _ = error "Error de parseo"
 
--- parseCondOrApp :: String -> [(SASA, SASA)] -> SASA
--- parseCondOrApp "cond" clauses = CondS clauses
--- parseCondOrApp name clauses = AppS (IdS name) (CondS clauses)
 
 }
--- src/Test.hs
+++ src/Test.hs
@@ -37,67 +37,68 @@ makeTest input expected label =
     putStrLn "--------------------------------------------------------------------------------------"
 
 -- Pruebas
-testSumaBinaria     = makeTest "(+ 2 3)"       (Num 5)  "suma binaria"
-testSumaVariadica   = makeTest "(+ 1 2 3 4)"   (Num 10) "suma variádica"
+testSumaBinaria             = makeTest "(+ 2 3)"       (Num 5)  "suma binaria"
+testSumaVariadica           = makeTest "(+ 1 2 3 4)"   (Num 10) "suma variádica"
 
-testRestaBinaria    = makeTest "(- 10 4)"      (Num 6)  "resta binaria"
-testRestaVariadica  = makeTest "(- 20 5 3)"    (Num 12) "resta variádica"
+testRestaBinaria            = makeTest "(- 10 4)"      (Num 6)  "resta binaria"
+testRestaVariadica          = makeTest "(- 20 5 3)"    (Num 12) "resta variádica"
 
-testMultBinaria     = makeTest "(* 3 4)"       (Num 12) "multiplicación binaria"
-testMultVariadica   = makeTest "(* 2 3 4 5)"   (Num 120) "multiplicación variádica"
+testMultBinaria             = makeTest "(* 3 4)"       (Num 12) "multiplicación binaria"
+testMultVariadica           = makeTest "(* 2 3 4 5)"   (Num 120) "multiplicación variádica"
 
-testDivBinaria      = makeTest "(/ 20 5)"      (Num 4)  "división binaria"
-testDivVariadica    = makeTest "(/ 100 2 5)"   (Num 10) "división variádica"
+testDivBinaria              = makeTest "(/ 20 5)"      (Num 4)  "división binaria"
+testDivVariadica            = makeTest "(/ 100 2 5)"   (Num 10) "división variádica"
 
-testSqrt = makeTest "(sqrt 9)" (Num 3) "raíz cuadrada de 9"
+testSqrt                    = makeTest "(sqrt 9)" (Num 3) "raíz cuadrada de 9"
 
-testExpt = makeTest "(expt 2 3)" (Num 8) "2 elevado a la 3"
+testExpt                    = makeTest "(expt 2 3)" (Num 8) "2 elevado a la 3"
+testExptVariadico           = makeTest "(expt 2 3 2)" (Num 512) "expt variádico como 2^(3^2)"
 
-testNotTrue = makeTest "(not #t)" (Boolean False) "negación de verdadero"
-testNotFalse = makeTest "(not #f)" (Boolean True) "negación de falso"
+testNotTrue                 = makeTest "(not #t)" (Boolean False) "negación de verdadero"
+testNotFalse                = makeTest "(not #f)" (Boolean True) "negación de falso"
 
-testIgualBinario     = makeTest "(= 5 5)"        (Boolean True) "igualdad binaria"
-testIgualVariadico   = makeTest "(= 2 2 2 2)"    (Boolean True) "igualdad variádica"
+testIgualBinario            = makeTest "(= 5 5)"        (Boolean True) "igualdad binaria"
+testIgualVariadico          = makeTest "(= 2 2 2 2)"    (Boolean True) "igualdad variádica"
 
-testMayorBinario     = makeTest "(> 7 3)"        (Boolean True) "mayor que binaria"
-testMayorVariadico   = makeTest "(> 9 5 2)"      (Boolean True) "mayor que variádica"
+testMayorBinario            = makeTest "(> 7 3)"        (Boolean True) "mayor que binaria"
+testMayorVariadico          = makeTest "(> 9 5 2)"      (Boolean True) "mayor que variádica"
 
-testMenorBinario     = makeTest "(< 3 7)"        (Boolean True) "menor que binaria"
-testMenorVariadico   = makeTest "(< 1 2 3 4)"    (Boolean True) "menor que variádica"
+testMenorBinario            = makeTest "(< 3 7)"        (Boolean True) "menor que binaria"
+testMenorVariadico          = makeTest "(< 1 2 3 4)"    (Boolean True) "menor que variádica"
 
-testMayorIgualBin    = makeTest "(>= 5 5)"       (Boolean True) "mayor o igual binaria"
-testMayorIgualVar    = makeTest "(>= 9 9 8 7)"   (Boolean True) "mayor o igual variádica"
+testMayorIgualBin           = makeTest "(>= 5 5)"       (Boolean True) "mayor o igual binaria"
+testMayorIgualVar           = makeTest "(>= 9 9 8 7)"   (Boolean True) "mayor o igual variádica"
 
-testMenorIgualBin    = makeTest "(<= 5 5)"       (Boolean True) "menor o igual binaria"
-testMenorIgualVar    = makeTest "(<= 1 2 2 3)"   (Boolean True) "menor o igual variádica"
+testMenorIgualBin           = makeTest "(<= 5 5)"       (Boolean True) "menor o igual binaria"
+testMenorIgualVar           = makeTest "(<= 1 2 2 3)"   (Boolean True) "menor o igual variádica"
 
-testDistintoBinario  = makeTest "(!= 5 3)"       (Boolean True) "diferente binaria"
-testDistintoVariadico= makeTest "(!= 1 2 3 4)"   (Boolean True) "diferente variádica"
+testDistintoBinario         = makeTest "(!= 5 3)"       (Boolean True) "diferente binaria"
+testDistintoVariadico       = makeTest "(!= 1 2 3 4)"   (Boolean True) "diferente variádica"
 
-testPairFst = makeTest "(fst (1, #t))" (Num 1) "proyección fst de par (1, #t)"
-testPairSnd = makeTest "(snd (3, 5))"  (Num 5) "proyección snd de par (3, 5)"
+testPairFst                 = makeTest "(fst (1, #t))" (Num 1) "proyección fst de par (1, #t)"
+testPairSnd                 = makeTest "(snd (3, 5))"  (Num 5) "proyección snd de par (3, 5)"
 
-testLetSimple = makeTest "(let ((x 5)) (+ x 2))" (Num 7) "let con una asignación"
-testLetVariadico = makeTest "(let ((x 2) (y 3)) (+ x y))" (Num 5) "let variádico sin dependencia"
+testLetSimple               = makeTest "(let ((x 5)) (+ x 2))" (Num 7) "let con una asignación"
+testLetVariadico            = makeTest "(let ((x 2) (y 3)) (+ x y))" (Num 5) "let variádico sin dependencia"
 
-testLetStarSimple = makeTest "(let* ((x 4)) (* x 2))" (Num 8) "let* con una asignación"
-testLetStarVariadico = makeTest "(let* ((x 3) (y (+ x 1))) (+ x y))" (Num 7) "let* variádico con dependencia"
+testLetStarSimple           = makeTest "(let* ((x 4)) (* x 2))" (Num 8) "let* con una asignación"
+testLetStarVariadico        = makeTest "(let* ((x 3) (y (+ x 1))) (+ x y))" (Num 7) "let* variádico con dependencia"
 
-testIfBoolean = makeTest "(if #t 42 0)" (Num 42) "if con condición verdadera"
-testIfFalse = makeTest "(if #f 42 0)" (Num 0) "if con condición falsa"
+testIfBoolean               = makeTest "(if #t 42 0)" (Num 42) "if con condición verdadera"
+testIfFalse                 = makeTest "(if #f 42 0)" (Num 0) "if con condición falsa"
 
-testListaVacia       = makeTest "nil" (eval (desugar (parser (lexer "nil")))) "lista vacía"
-testListaNumeros     = makeTest "(cons 1 (cons 2 (cons 3 (cons 4 nil))))" (eval (desugar (parser (lexer "(cons 1 (cons 2 (cons 3 (cons 4 nil))))")))) "lista de números"
-testHeadLista        = makeTest "(head (cons 1 (cons 2 (cons 3 nil))))" (Num 1) "head de lista construida con cons"
-testTailLista        = makeTest "(tail (cons 3 (cons 5 (cons 6 nil))))" (eval (desugar (parser (lexer "(cons 5 (cons 6 nil) )")))) "tail de lista construida con cons"
+testListaVacia              = makeTest "nil" (eval (desugar (parser (lexer "nil")))) "lista vacía"
+testListaNumeros            = makeTest "(cons 1 (cons 2 (cons 3 (cons 4 nil))))" (eval (desugar (parser (lexer "(cons 1 (cons 2 (cons 3 (cons 4 nil))))")))) "lista de números"
+testHeadLista               = makeTest "(head (cons 1 (cons 2 (cons 3 nil))))" (Num 1) "head de lista construida con cons"
+testTailLista               = makeTest "(tail (cons 3 (cons 5 (cons 6 nil))))" (eval (desugar (parser (lexer "(cons 5 (cons 6 nil) )")))) "tail de lista construida con cons"
 
-testCondPrimeraVerdadera = makeTest "(cond [(= 0 0) 42] [(= 1 1) 99] [else 0])" (Num 42) "cond con primera cláusula verdadera"
-testCondSegundaVerdadera = makeTest "(cond [(= 1 2) 10] [(= 3 3) 20] [else 30])" (Num 20) "cond con segunda cláusula verdadera"
-testCondElse = makeTest "(cond [(= 1 2) 10] [(> 2 5) 20] [else 99])" (Num 99) "cond sin coincidencias, ejecuta else"
+testCondPrimeraVerdadera    = makeTest "(cond [(= 0 0) 42] [(= 1 1) 99] [else 0])" (Num 42) "cond con primera cláusula verdadera"
+testCondSegundaVerdadera    = makeTest "(cond [(= 1 2) 10] [(= 3 3) 20] [else 30])" (Num 20) "cond con segunda cláusula verdadera"
+testCondElse                = makeTest "(cond [(= 1 2) 10] [(> 2 5) 20] [else 99])" (Num 99) "cond sin coincidencias, ejecuta else"
 
-testLambdaSimple = makeTest "((lambda (x) (+ x 1)) 4)" (Num 5) "lambda con un parámetro"
-testLambdaBinaria = makeTest "((lambda (x y) (* x y)) 3 4)" (Num 12) "lambda con dos parámetros"
-testLambdaVariadica = makeTest "((lambda (x y z) (+ x y z)) 1 2 3)" (Num 6) "lambda con tres parámetros"
+testLambdaSimple            = makeTest "((lambda (x) (+ x 1)) 4)" (Num 5) "lambda con un parámetro"
+testLambdaBinaria           = makeTest "((lambda (x y) (* x y)) 3 4)" (Num 12) "lambda con dos parámetros"
+testLambdaVariadica         = makeTest "((lambda (x y z) (+ x y z)) 1 2 3)" (Num 6) "lambda con tres parámetros"
 
 
 -- Agrupar
@@ -107,7 +108,7 @@ tests = TestList
   , testRestaBinaria, testRestaVariadica
   , testMultBinaria, testMultVariadica
   , testDivBinaria, testDivVariadica
-  , testSqrt, testExpt
+  , testSqrt, testExpt, testExptVariadico
   , testNotTrue, testNotFalse
   , testIgualBinario, testIgualVariadico
   , testMayorBinario, testMayorVariadico
