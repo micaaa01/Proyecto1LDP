1a652d421b443684810e9afdeeae099404327382
unknown <pollispaps49@gmail.com>
2025-10-30 13:30:31 -0600

    Arreglando desugar junto con eval

--- src/Desugar.hs
+++ src/Desugar.hs
@@ -1,15 +1,28 @@
 module Desugar where
 import Grammar (SASA(..))
 
+data Op
+  = AddOp | SubOp | MulOp | DivOp
+  | EqOp | NeOp | LtOp | LeOp | GtOp | GeOp
+  | ExptOp
+  deriving (Show, Eq)
+
 data ASA = Num Int
                 | Boolean Bool
                 | Id String
-                | Add ASA ASA
-                | Sub ASA ASA
-                | Mul ASA ASA
-                | Div ASA ASA
+                | BinOp Op ASA ASA
+--                | Add ASA ASA
+--                | Sub ASA ASA
+--                | Mul ASA ASA
+--                | Div ASA ASA
+--                | Eq ASA ASA
+--                | Lt ASA ASA
+--                | Gt ASA ASA
+--                | Le ASA ASA
+--                | Ge ASA ASA
+--                | Ne ASA ASA
                 | Sqrt ASA
-                | Expt ASA ASA
+--                | Expt ASA ASA
                 | Not ASA
                 | Pair ASA ASA
                 | Fst ASA
@@ -20,44 +33,53 @@ data ASA = Num Int
                 deriving (Show, Eq)
 
 desugar :: SASA -> ASA
-desugar (NumS n)                = Num n
-desugar (BooleanS b)            = Boolean b
-desugar (IdS x)                 = Id x
-desugar (NotS e)                = Not (desugar e)
-desugar (AddListS es)           = foldl1 Add (map desugar es)
-desugar (SubListS es)           = foldl1 Sub (map desugar es)
-desugar (MulListS es)           = foldl1 Mul (map desugar es)
-desugar (DivListS es)           = foldl1 Div (map desugar es)
-desugar (EqListS es)            = foldl1 (\a b -> App (App (Id "=") a) b) (map desugar es)
-desugar (LtListS es)            = foldl1 (\a b -> App (App (Id "<") a) b) (map desugar es)
-desugar (GtListS es)            = foldl1 (\a b -> App (App (Id ">") a) b) (map desugar es)
-desugar (LeListS es)            = foldl1 (\a b -> App (App (Id "<=") a) b) (map desugar es)
-desugar (GeListS es)            = foldl1 (\a b -> App (App (Id ">=") a) b) (map desugar es)
-desugar (NeListS es)            = foldl1 (\a b -> App (App (Id "!=") a) b) (map desugar es)
-desugar (FunListS [] body)      = desugar body
-desugar (FunListS (x:xs) body)  = Fun x (desugar (FunListS xs body))
-desugar (SqrtS e)               = Sqrt (desugar e)
-desugar (ExptS e1 e2)           = Expt (desugar e1) (desugar e2)
-desugar (ExptListS es)          = foldr1 Expt (map desugar es)
-desugar (PairS e1 e2)           = Pair (desugar e1) (desugar e2)
-desugar (FstS e)                = Fst (desugar e)
-desugar (SndS e)                = Snd (desugar e)
-desugar (IfS c t f)             = If (desugar c) (desugar t) (desugar f)
-desugar (FunS x body)           = Fun x (desugar body)
-desugar (AppS e1 e2)            = App (desugar e1) (desugar e2)
-desugar (LetS [] body)          = desugar body
-desugar (LetS [(x, e)] body)    = App (Fun x (desugar body)) (desugar e)
-desugar (LetS ((x,e):rest) body) = desugar (LetS [(x,e)] (LetS rest body))
-desugar (LetStarS [] body)      = desugar body
+desugar (NumS n)                     = Num n
+desugar (BooleanS b)                 = Boolean b
+desugar (IdS x)                      = Id x
+desugar (NotS e)                     = Not (desugar e)
+desugar (AddListS es)                = foldl1 (BinOp AddOp) (map desugar es)
+desugar (SubListS es)                = foldl1 (BinOp SubOp) (map desugar es)
+desugar (MulListS es)                = foldl1 (BinOp MulOp) (map desugar es)
+desugar (DivListS es)                = foldl1 (BinOp DivOp) (map desugar es)
+desugar (EqListS es) = chainCompare EqOp (map desugar es)
+desugar (LtListS es) = chainCompare LtOp (map desugar es)
+desugar (GtListS es) = chainCompare GtOp (map desugar es)
+desugar (LeListS es) = chainCompare LeOp (map desugar es)
+desugar (GeListS es) = chainCompare GeOp (map desugar es)
+desugar (NeListS es) = chainCompare NeOp (map desugar es)
+
+desugar (FunListS [] body)           = desugar body
+desugar (FunListS (x:xs) body)       = Fun x (desugar (FunListS xs body))
+desugar (SqrtS e)                    = Sqrt (desugar e)
+desugar (ExptS e1 e2)                = BinOp ExptOp (desugar e1) (desugar e2)
+desugar (ExptListS es)               = foldr1 (BinOp ExptOp) (map desugar es)
+desugar (PairS e1 e2)                = Pair (desugar e1) (desugar e2)
+desugar (FstS e)                     = Fst (desugar e)
+desugar (SndS e)                     = Snd (desugar e)
+desugar (IfS c t f)                  = If (desugar c) (desugar t) (desugar f)
+desugar (FunS x body)                = Fun x (desugar body)
+desugar (AppS e1 e2)                 = App (desugar e1) (desugar e2)
+desugar (LetS [] body)               = desugar body
+desugar (LetS [(x, e)] body)         = App (Fun x (desugar body)) (desugar e)
+desugar (LetS ((x,e):rest) body)     = desugar (LetS [(x,e)] (LetS rest body))
+desugar (LetStarS [] body)           = desugar body
 desugar (LetStarS ((x,e):rest) body) = desugar (LetS [(x,e)] (LetStarS rest body))
-desugar (ListS []) = Id "nil"
-desugar (ListS (x:xs))          = App (App (Id "cons") (desugar x)) (desugar (ListS xs))
-desugar (HeadS e)               = App (Id "head") (desugar e)
-desugar (TailS e)               = App (Id "tail") (desugar e)
-desugar (CondS clauses)         = desugarCond clauses
+desugar (ListS [])                   = Id "nil"
+desugar (ListS (x:xs))               = App (App (Id "cons") (desugar x)) (desugar (ListS xs))
+desugar (HeadS e)                    = App (Id "head") (desugar e)
+desugar (TailS e)                    = App (Id "tail") (desugar e)
+desugar (CondS clauses)              = desugarCond clauses
 
 desugarCond :: [(SASA, SASA)] -> ASA
-desugarCond []              = error "cond sin cláusulas"
-desugarCond [(g, e)]        = desugar e  -- última cláusula (else)
-desugarCond ((g, e):rest)   = If (desugar g) (desugar e) (desugarCond rest)
+desugarCond []                       = error "cond sin cláusulas"
+desugarCond [(g, e)]                 = desugar e  
+desugarCond ((g, e):rest)            = If (desugar g) (desugar e) (desugarCond rest)
 
+chainCompare :: Op -> [ASA] -> ASA
+chainCompare _ [] = Boolean True
+chainCompare _ [_] = Boolean True
+chainCompare op (x:y:rest) =
+  let cmp = BinOp op x y
+  in case rest of
+       []     -> cmp
+       (_:_)  -> BinOp EqOp cmp (chainCompare EqOp (y:rest))
--- src/Eval.hs
+++ src/Eval.hs
@@ -1,5 +1,5 @@
 module Eval where
-import Desugar (ASA (..))
+import Desugar (ASA (..), Op(..))
 
 type Env = [(String, ASA)]
 
@@ -7,47 +7,25 @@ smallStep :: ASA -> Env -> (ASA, Env)
 smallStep (Id i) env = (lookupEnv i env, env)
 smallStep (Num n) env = (Num n, env)
 smallStep (Boolean b) env = (Boolean b, env)
-smallStep (Add (Num n) (Num m)) env = (Num (n+m), env)
-smallStep (Add (Num n) d) env =  
-    let (d', env') = smallStep d env
-    in (Add (Num n) d', env')
-smallStep (Add i d) env =
-    let (i', env') = smallStep i env
-    in (Add i' d, env')
-smallStep (Sub (Num n) (Num m)) env = (Num (n-m), env)
-smallStep (Sub (Num n) d) env =  
-    let (d', env') = smallStep d env
-    in (Sub (Num n) d', env')
-smallStep (Sub i d) env =
-    let (i', env') = smallStep i env
-    in (Sub i' d, env')
-smallStep (Mul (Num n) (Num m)) env = (Num (n*m), env)
-smallStep (Mul (Num n) d) env =  
-    let (d', env') = smallStep d env
-    in (Mul (Num n) d', env')
-smallStep (Mul i d) env =
-    let (i', env') = smallStep i env
-    in (Mul i' d, env')
-smallStep (Div (Num n) (Num m)) env = (Num (n `div` m), env)
-smallStep (Div (Num n) d) env =  
-    let (d', env') = smallStep d env
-    in (Div (Num n) d', env')
-smallStep (Div i d) env =
-    let (i', env') = smallStep i env
-    in (Div i' d, env')
+
+smallStep (BinOp op v1 v2) env
+  | isValue v1 && isValue v2 = (evalBinOp op v1 v2, env)
+smallStep (BinOp op v1 e2) env
+  | isValue v1 = 
+      let (e2', env') = smallStep e2 env
+      in (BinOp op v1 e2', env')
+smallStep (BinOp op e1 e2) env =
+  let (e1', env') = smallStep e1 env
+  in (BinOp op e1' e2, env')
+
+
 smallStep (Sqrt (Num n)) env =
     let r = sqrt (fromIntegral n :: Double)
     in (Num (floor r), env)
 smallStep (Sqrt e) env =
     let (e', env') = smallStep e env
     in (Sqrt e', env')
-smallStep (Expt (Num n) (Num m)) env = (Num (n^m), env)
-smallStep (Expt (Num n) d) env =  
-    let (d', env') = smallStep d env
-    in (Expt (Num n) d', env')
-smallStep (Expt i d) env =
-    let (i', env') = smallStep i env
-    in (Expt i' d, env')
+
 smallStep (Not (Boolean b)) env = (Boolean (not b), env)
 smallStep (Not e) env =
     let (e', env') = smallStep e env
@@ -92,3 +70,17 @@ isValue (Boolean _) = True
 isValue (Fun _ _) = True
 isValue (Pair v1 v2) = isValue v1 && isValue v2
 isValue _ = False
+
+evalBinOp :: Op -> ASA -> ASA -> ASA
+evalBinOp AddOp (Num x) (Num y) = Num (x + y)
+evalBinOp SubOp (Num x) (Num y) = Num (x - y)
+evalBinOp MulOp (Num x) (Num y) = Num (x * y)
+evalBinOp DivOp (Num x) (Num y) = Num (x `div` y)
+evalBinOp ExptOp (Num x) (Num y) = Num (x ^ y)
+evalBinOp EqOp  x y = Boolean (x == y)
+evalBinOp NeOp  x y = Boolean (x /= y)
+evalBinOp LtOp  (Num x) (Num y) = Boolean (x < y)
+evalBinOp LeOp  (Num x) (Num y) = Boolean (x <= y)
+evalBinOp GtOp  (Num x) (Num y) = Boolean (x > y)
+evalBinOp GeOp  (Num x) (Num y) = Boolean (x >= y)
+evalBinOp _ _ _ = error "Operación binaria inválida"
